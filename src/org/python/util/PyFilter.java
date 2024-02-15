0f4b79bb5a8dde7557c0e389a79a4d0994ef15fe
Otmar Humbel <otmar.humbel@bison-group.com>
2024-02-15 15:46:29 +0100

    jakarta migration

--- src/org/python/util/PyFilter.java
+++ src/org/python/util/PyFilter.java
@@ -2,13 +2,13 @@ package org.python.util;
 
 import java.io.File;
 import java.io.IOException;
-import javax.servlet.Filter;
-import javax.servlet.FilterChain;
-import javax.servlet.FilterConfig;
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.FilterConfig;
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
 
 import org.python.core.PyException;
 import org.python.util.PythonInterpreter;
