d042c4e24f40f76553db00c79670a3bbe8f939e6
Otmar Humbel <ohumbel@gmail.com>
2024-08-09 18:03:17 +0200

    Adapt PyStringTest to the new Py.newString(s) implementation
    Use the PyString constructor directly to produce the IllegalAccessException.
    And the Turkish letters fit fine into Py.newString(s), since there is a PyUnicode created.

--- tests/java/org/python/core/PyStringTest.java
+++ tests/java/org/python/core/PyStringTest.java
@@ -8,21 +8,22 @@ import org.junit.Test;
 /** Odd tests of the Python {@code str} implementation that are best done from Java. */
 public class PyStringTest {
 
+    public static final String RIGHT_SINGLE_QUOTATION_MARK = "\u2019"; // needs more than 8 bits
+    public static final String JEANNE_DARC = "Jeanne d" + RIGHT_SINGLE_QUOTATION_MARK + "Arc";
+
     /**
-     * Construction of a {@code PyString} from a {@code String} with characters > {@code 0xff}
-     * results in an {@code IllegalArgumentException} that quotes a safely-escaped version of the
-     * string.
+     * Construction of a {@code PyString} from a {@code String} with characters > {@code 0xff} results in an
+     * {@code IllegalArgumentException} that quotes a safely-escaped version of the string.
      */
     @Test
     public void preventNonBytePyString() {
         try {
-            // Quercus cerris (turkey oak) in Turkish. https://tr.wikipedia.org
-            PyString s = Py.newString("Saçlı meşe");
+            new PyString(JEANNE_DARC);
+            fail("Expected an IllegalArgumentException which was not thrown");
         } catch (IllegalArgumentException iae) {
             String m = iae.getMessage();
-            assertTrue("Quotes message", m.contains("Sa\\xe7l\\u0131 me\\u015fe"));
-            return;
+            assertTrue("Expected right single quotation mark not to fit into 8 bits of a PyString",
+                            m.contains("Jeanne d\\u2019Arc"));
         }
-        fail("Expected exception not thrown");
     }
 }
